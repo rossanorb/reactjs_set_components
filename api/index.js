2efb0880ac97a767add5ddff68693b3d0af4b64e
Rossano <rossanorb@gmail.com>
2023-02-17 15:20:59 -0300

    Setting Cors rule

--- api/index.js
+++ api/index.js
@@ -4,7 +4,9 @@ const cors = require('cors')
 const router = require("./routers/index")
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:"http://localhost:3000"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
