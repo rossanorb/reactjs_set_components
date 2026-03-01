24269dd81eab29f6171d39c282dbbe77c85b4e0d
Rossano <rossanorb@gmail.com>
2026-03-01 14:59:35 -0300

    chore: configure API server with env vars

--- api/index.js
+++ api/index.js
@@ -1,19 +1,24 @@
+require('dotenv').config();
 const http = require('http');
-const express = require('express')
-const cors = require('cors')
-const router = require("./routers/index")
+const express = require('express');
+const cors = require('cors');
+const router = require('./routers/index');
 const app = express();
 
-app.use(cors({
-    origin:"http://localhost:3000"
-}));
+const port = process.env.PORT || 3000;
+const url = process.env.API_URL || 'http://localhost';
+
+app.use(
+  cors({
+    origin: `${url}:${port}`,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-router(app)
-  
-var server = http.createServer(app);
-server.listen(4000, () => {
-    console.log("Server started")
-});
+router(app);
 
+var server = http.createServer(app);
+server.listen(port, () => {
+  console.log(`Server started ${url}:${port}`);
+});
